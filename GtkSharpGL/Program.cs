d3419e3207e51f5e6892b56b6897ca1d6968766b
James Gregory <james@james.id.au>
2017-06-02 17:27:06 -0700

    Initial import.

---
+++ GtkSharpGL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gtk;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace GtkSharpGL
+{
+	public class Program
+	{
+		public static void Main(String[] args) {
+
+			Toolkit.Init ();
+			Application.Init ();
+
+			var window = new Window("GtkSharp GL Test Application");
+			var glArea = new GLArea ();
+
+			glArea.Realized += (object sender, EventArgs e) => {
+				glArea.MakeCurrent();
+				var vendor = GL.GetString(StringName.Vendor);
+				Console.WriteLine($"Vendor is {vendor}");
+			};
+
+			window.Add (glArea);
+			window.ShowAll ();
+
+			Application.Run ();
+		}
+	}
+}
